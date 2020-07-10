e79d235cd3fd30c73fc38563984103acb397f45d
anthonyvigz <anthonyvigz@gmail.com>
2020-07-10 11:07:24 -0400

    parsed csv data using papaparse library to array

--- dup-filter/src/App.js
+++ dup-filter/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import csvfile from "./testFiles/normal.csv";
+import Papa from "papaparse";
 
 function App() {
   // State declaration for the current CSV file
@@ -8,8 +9,27 @@ function App() {
   // State declaration for the unfiltered parsed list
   const [original, setOriginal] = useState([]);
 
+  // Parsing CSV file through useEffect just in case anything
+  // changes on re-render
+  useEffect(() => {
+    // async function to make sure the file is read correctly
+    async function getData() {
+      const response = await fetch(file); // store the response from the request
+      const reader = response.body.getReader();
+      const result = await reader.read(); // result is raw array with utf-8 value
+      const decoder = new TextDecoder("utf-8"); // set up decoder to read the result
+      const csv = decoder.decode(result.value); // the csv text
+      const results = Papa.parse(csv, { header: true }); // papaparse parses readable CSV data
+      const original = results.data; // access JSON array from papaparse result
+      setOriginal(original); // sets the JSON array to our state for the app
+    }
+    getData();
+  }, []);
+
+  console.log(original);
+
   return (
-    <div className="App">
+    <div className="mainApp">
       {original.map((person, i) => {
         return <h1>{person["first_name"]}</h1>;
       })}
