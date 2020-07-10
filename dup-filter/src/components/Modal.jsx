bf7ea0972f1b3eb517c30f09ed6c76a15046c870
anthonyvigz <anthonyvigz@gmail.com>
2020-07-10 12:31:30 -0400

    created modal cards for each user

--- dup-filter/src/components/Modal.jsx
+++ dup-filter/src/components/Modal.jsx
@@ -6,7 +6,25 @@ export default function Modal(props) {
       onClick={() => props.showModal(false)}
       className={props.visible ? "modal" : "modal hide"}
     >
-      <div className="grayedBackdrop">{props.person["last_name"]}</div>
+      <div className="grayedBackdrop">
+        <div className="modalBlock">
+          <h1 id="idtext">{props.person["id"]}</h1>
+          <h1 id="nametext">
+            {props.person["first_name"]} {props.person["last_name"]}
+          </h1>
+          <h1 id="companytext">{props.person["company"]}</h1>
+          <h1 id="emailtext">{props.person["email"]}</h1>
+          <div className="address" style={{ background: props.color }}>
+            <h1>{props.person["address1"]}</h1>
+            <h1>{props.person["address2"]}</h1>
+            <h1>
+              {props.person["city"]}, {props.person["state"]}
+            </h1>
+            <h1>{props.person["zip"]}</h1>
+          </div>
+          <h1 id="phonetext">Phone: {props.person["phone"]}</h1>
+        </div>
+      </div>
     </div>
   );
 }
--- dup-filter/src/components/Person.jsx
+++ dup-filter/src/components/Person.jsx
@@ -15,7 +15,12 @@ export default function Person(props) {
       <h1>{props.person["id"]}</h1>
       <h1>{props.person["first_name"]}</h1>
       <h1>{props.person["last_name"]}</h1>
-      <Modal visible={modal} person={props.person} showModal={showModal} />
+      <Modal
+        visible={modal}
+        person={props.person}
+        color={props.color}
+        showModal={showModal}
+      />
     </div>
   );
 }
