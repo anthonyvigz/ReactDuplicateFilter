d469a958bd4ee6a7f5cd5915b2ef161e3c2014dc
anthonyvigz <anthonyvigz@gmail.com>
2020-07-10 10:49:43 -0400

    set up quick jsx to render list once parsed for test

--- dup-filter/src/App.js
+++ dup-filter/src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import csvfile from "./testFiles/normal.csv";
 
 function App() {
-  return <div className="App">main app here</div>;
+  // State declaration for the current CSV file
+  const [file, setFile] = useState(csvfile);
+
+  // State declaration for the unfiltered parsed list
+  const [original, setOriginal] = useState([]);
+
+  return (
+    <div className="App">
+      {original.map((person, i) => {
+        return <h1>{person["first_name"]}</h1>;
+      })}
+    </div>
+  );
 }
 
 export default App;
